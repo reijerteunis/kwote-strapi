995a2fc915cc7b97076ed736cade9cd545cd7133
Ruud van Engelenhoven <ruud@Ruuds-Mac-mini.local>
2021-12-10 07:24:31 +0100

    Datatype changes

--- config/database.js
+++ config/database.js
@@ -1,11 +1,35 @@
-const path = require('path');
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: env("DATABASE_HOST", "localhost"),
+//         port: env.int("DATABASE_PORT", 5432),
+//         database: env("DATABASE_NAME", "kwote"),
+//         username: env("DATABASE_USERNAME", "roula"),
+//         password: env("DATABASE_PASSWORD", "password"),
+//         schema: env("DATABASE_SCHEMA", "public"),
+//       },
+//       options: {},
+//     },
+//   },
+// });
+
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'kwote'),
+      user: env('DATABASE_USERNAME', 'roula'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env('DATABASE_SSL', false)
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
