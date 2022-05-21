028b7f590ef675f711bfb998f4d1c24c663108d8
Ruud <info@ruud.tech>
2022-05-21 15:37:05 +0200

    upgrade

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'kwote'),
-      user: env('DATABASE_USERNAME', 'roula'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'roula'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: env('DATABASE_SSL', false)
     },
